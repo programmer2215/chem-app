1a0e57c2530410d8e68174cc082fd21b51209052
Mark Madhukar <markmadhukar03@gmail.com>
2021-02-23 10:52:14 +0530

    deployed my react app to github pages!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Element1 from'./element.js';
 
 function App() {
+  
+  var symbol = "";
+  
+  const [data, setData] = useState([]);
+
+  const getElements = async (element_name) =>{
+    const response = await fetch(`https://neelpatel05.pythonanywhere.com/element/atomicname?atomicname=${element_name}`);
+    const data_raw = await response.json();
+    setData(data_raw);
+    
+  }
+  
+  
+  
+     const search = () => {
+      getElements(document.getElementById("search-bar").value);
+     }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">Elemental Data</h1>
+      <div className="search-pane">
+      <form>
+        <input id="search-bar" type="text" defaultValue="Hydrogen"/>
+        <button id="search-btn" type="button" onClick={search}>Search</button>
+      </form>
+      
+      </div>
+
+      <Element1 results={data}/>
+      
+    
     </div>
+    
+    
   );
 }
 
---
+++ src/element.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Element1 = (results) => {
+
+    const result_length = Object.keys(results.results).length;
+
+
+
+    const data = results.results;
+
+    return(
+        <div id="results-container">
+            <h2 id="ERROR"></h2>
+            <h2 >Atomic Mass:  {data.atomicMass}</h2>
+            <h2 >Atomic Number:  {data.atomicNumber}</h2>
+            <h2 >Atomic Radius:  {data.atomicRadius}</h2>
+            <h2 >Boiling Point:  {data.boilingPoint}</h2>
+            <h2 >Bonding Type:  {data.bondingType}</h2>
+            <h2 >CpkHex Color:  {data.cpkHexColor}</h2>
+            <h2 >Density:  {data.density}</h2>
+            <h2 >Electron Affinity:  {data.electronAffinity}</h2>
+            <h2 >Electronegativity:  {data.electronegativity}</h2>
+            <h2 id="ec">Electronic Configuration: <br></br> {data.electronicConfiguration}</h2>
+            <h2 >Group Block:  {data.groupBlock}</h2>
+            <h2 >ion Radius:  {data.ionRadius}</h2>
+            <h2 >ionization Energy:  {data.ionizationEnergy}</h2>
+            <h2 >melting Point:  {data.meltingPoint}</h2>
+            <h2 >name:  {data.name}</h2>
+            <h2 >oxidation States:  {data.oxidationStates}</h2>
+            <h2 >standard State:  {data.standardState}</h2>
+            <h2 >symbol:  {data.symbol}</h2>
+            <h2 >van Del Waals Radius:  {data.vanDelWaalsRadius}</h2>
+            <h2 >year Discovered:  {data.yearDiscovered}</h2>
+        </div>
+    )
+}
+
+export default Element1;
